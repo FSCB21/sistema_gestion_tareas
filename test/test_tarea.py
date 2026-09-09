15491d73f4270e7d86090dc3ad1003a099ffc17a
Freddy Calderon <freddystibencb@gmail.com>
2026-09-08 20:48:13 -0500

    configuracion basica de test, validacion de funcionalidad

---
+++ test/test_tarea.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestEntornoPruebas(unittest.TestCase):
+
+  def test_verificacion_entorno(self):
+    """Prueba básica para comprobar que el test runner ejecuta las pruebas."""
+    self.assertTrue(True)
+
+
+if __name__ == "__main__":
+  unittest.main()
